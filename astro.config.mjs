c3145736e1cb45ef05437b5516e7614dd10e2953
Declan Arkins <dec.arkins@gmail.com>
2023-10-02 12:35:28 +0100

    adding image optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+// import NetlifyCMS from "astro-netlify-cms";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://dextro.netlify.app",
+  integrations: [mdx(), sitemap()],
 });
